81ab8454cc600b1aaa3f3431f3479cd971abb5b1
Lucas Esteban <lucasestebanlps@gmail.com>
2023-12-28 19:01:57 +0100

    firebase now its configurated and works correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:applichiamoci/app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  // Widgets binding
+  WidgetsFlutterBinding.ensureInitialized();
+  // Todo: init local storage
+  // Todo: awatit native splash
+  // Todo: initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Todo: initialize authentication
 
-void main() {
   runApp(const App());
 }
